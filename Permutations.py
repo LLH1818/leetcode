81b156121ca33d719669228b19c8d04f86363ddb
曹佳禾 <caojiahe1818@gmail.com>
2018-11-18 23:04:31 +0800

    2018.11.18

---
+++ Permutations.py
@@ -0,0 +1,15 @@
+def permute(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    perms = [[]]
+    for n in nums:
+        new_perms = []
+        for perm in perms:
+            for i in range(len(perm) + 1):
+                new_perms.append(perm[:i] + [n] + perm[i:])  ###insert n
+        perms = new_perms
+    return perms
+
+permute([1, 2, 3])
