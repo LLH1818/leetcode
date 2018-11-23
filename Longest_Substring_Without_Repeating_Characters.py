e284feb380666476c21517d30984c45bf79a67b8
曹佳禾 <caojiahe1818@gmail.com>
2018-11-23 14:42:19 +0800

    2018.11.23

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -1,7 +1,22 @@
-def lengthOfLongestSubstring(s):
+def lengthOfLongestSubstring(s):    # 快速的解法和我的思想一直，它利用字典，查找速度变快
     """
     :type s: str
     :rtype: int
     """
-    
+    if not s:
+        return 0
+    cache = []
+    result = []
+    for i in range(s.__len__()):
+        if s[i] not in cache:
+            cache.append(s[i])
+        else:
+            result.append(cache.__len__())
+            cache = cache[cache.index(s[i]) + 1:]
+            cache.append(s[i])
+        result.append(cache.__len__())
+    return max(result)
+
+
+print(lengthOfLongestSubstring("dvdf"))
 
--- Partition_Array_into_Disjoint_Intervals.py
+++ Partition_Array_into_Disjoint_Intervals.py
@@ -1,7 +1,57 @@
-def partitionDisjoint(A):
+def partitionDisjoint1(A):        #  一个一个比  太慢
     """
     :type A: List[int]
     :rtype: int
     """
     for i in range(A.__len__()):
+        black_flag = 0
+        left = A[: i + 1]
+        right = A[i + 1:]
+        for rnum in right:
+            for lnum in left:
+                if lnum > rnum:
+                    black_flag = 1
+                    break
+            if black_flag == 1:
+                break
+        if black_flag == 0:
+            return i + 1
+
+
+def partitionDisjoint2(A):     # 左边最大的和右边最小的比
+    for i in range(A.__len__()):
+        black_flag = 0
+        left = A[: i + 1]
+        right = A[i + 1:]
+        lmax = 0
+        rmin = right[0]
+        for lnum in left:
+            if lnum > lmax:
+                lmax = lnum
+        for rnum in right:
+            if rnum < rmin:
+                rmin = rnum
+        if lmax > rmin:
+            black_flag = 1
+        if black_flag == 0:
+            return i + 1
+
+
+def partitionDisjoint3(A):     # 重新审视规则，不用蛮力
+    lmax = A[0]
+    local_max = lmax
+    result = 0
+    for i in range(len(A) - 1):
+        if lmax > A[i]:
+            result = i
+            lmax = local_max
+        else:
+            local_max = max(A[i], local_max)
+    return result + 1
+
+
+
+
+
+print(partitionDisjoint3([24,11,49,80,63,8,61,22,73,85]))
 
