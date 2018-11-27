ae1798ac9b0caacaa9fbb4b475b0d36179a62e1d
曹佳禾 <caojiahe1818@gmail.com>
2018-11-27 14:28:05 +0800

    2018.11.27

--- Container_With_Most_Water.py
+++ Container_With_Most_Water.py
@@ -12,14 +12,25 @@ def maxArea1(height):
     return global_max
 
 
-def maxArea2(height):
+def maxArea2(height):   # 将长度这个参数固定下来，长度从最长逐渐缩短，深度必须要变深
     """
     :type height: List[int]
     :rtype: int
     """
+    if not height:
+        return 0
     i, j = 0, len(height) - 1
-    cache = []
-    for i in range()
+    cache = [(j - i) * min(height[i], height[j])]
+    for t in range(len(height) - 1):
+        if height[i] < height[j]:
+            i += 1
+            if height[i] > height[i - 1]:
+                cache.append(min(height[i], height[j]) * (j - i))
+        else:
+            j = j - 1
+            if height[j] > height[j + 1]:
+                cache.append(min(height[i], height[j]) * (j - i))
+    return(max(cache))
 
 
 
@@ -28,4 +39,6 @@ def maxArea2(height):
 
 
 
-print(maxArea1([1,8,6,2,5,4,8,3,7]))
+
+
+print(maxArea2([1,1]))
